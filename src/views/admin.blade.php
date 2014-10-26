69b7af3cee8d2111dd9b70474c9c7871734fa3cb
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-26 15:33:09 +0100

    Added admin view and controller

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -12,20 +12,32 @@
 
 	<div class="col-sm-10 col-md-10 col-lg-8"> 
 
+		<a href="{{ url('admin/page/create') }}" class="btn btn-lg btn-primary pull-right">
+			Create new page
+		</a>
+
 		<table class="table table-striped">
 			<thead>
 				<th>#</th>
 				<th>Slug</th>
 				<th>Name</th>
+				<th>
 			</thead>
 			<tbody>
+				@if(count($pages))
 				@foreach($pages as $page)
 				<tr>
 					<td>{{ $page->id }}</td>
 					<td>{{ $page->slug }}</td>
 					<td>{{ $page->name }}</td>
+					<td>
+						<a href="{{ url('admin/page/' . $page->slug) }}" class="btn btn-sm btn-warning">edit</a>
+					</td>
 				</tr>
 				@endforeach
+				@else
+					<p class="text-muted">No pages found.</p>
+				@endif
 			</tbody>
 		</table>
 
